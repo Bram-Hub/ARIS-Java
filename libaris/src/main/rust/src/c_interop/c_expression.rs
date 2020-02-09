ea791f30380b0a7d0b65df0e54d2b3dfb9486d35
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2020-02-09 16:37:53 -0500

    Update c_expression.rs

--- libaris/src/main/rust/src/c_interop/c_expression.rs
+++ libaris/src/main/rust/src/c_interop/c_expression.rs
@@ -1,5 +1,6 @@
 use super::*;
-use std::ffi::{CStr/*, CString*/};
+use std::ffi::CStr;
+//use std::ffi::CString;
 
 #[no_mangle] pub extern "C" fn aris_vec_expr_index(x: &Vec<Expr>, i: usize) -> Expr { x[i].clone() }
 #[no_mangle] pub extern "C" fn aris_vec_string_index(x: &Vec<String>, i: usize) -> String { x[i].clone() }
@@ -10,10 +11,6 @@ pub extern "C" fn aris_expr_parse(e: *const i8) -> *mut Expr {
     with_null_options(|| {
         let mut s = unsafe { CStr::from_ptr(e) }.to_string_lossy().into_owned();
         s.push('\n');
-        if let Ok((_, expr)) = parser::main(&s) {
-            Some(expr)
-        } else {
-            None
-        }
+        parser::main(&s).ok().map(|(_, expr)| expr)
     })
 }
